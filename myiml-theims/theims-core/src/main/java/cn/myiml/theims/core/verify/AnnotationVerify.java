322ccead033a9ed0a9b84d3f47c8ceda3519cede
yangzhoucyl <cleanlove7@163.com>
2021-04-14 15:13:08 +0800

    注解校验方法具体实现

--- myiml-theims/theims-core/src/main/java/cn/myiml/theims/core/verify/AnnotationVerify.java
+++ myiml-theims/theims-core/src/main/java/cn/myiml/theims/core/verify/AnnotationVerify.java
@@ -1,8 +1,27 @@
 package cn.myiml.theims.core.verify;
 
+import cn.myiml.theims.core.model.VerifyRulesConfigModel;
+import cn.myiml.theims.core.rule.load.AnnotationLoadRule;
+import cn.myiml.theims.core.rule.load.LoadVerifyRule;
+import cn.myiml.theims.core.verify.cache.VerifyRuleSingleton;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * 注解验证方式
  * @author yangzhou
  */
-public class AnnotationVerify extends ParamVerify{
+
+public class AnnotationVerify extends AbstractParamVerify {
+
+
+    @Override
+    public void verify(Object args, String route) {
+        if (args instanceof Map){
+            LoadVerifyRule<VerifyRulesConfigModel> loadVerifyRule = new AnnotationLoadRule();
+            List<VerifyRulesConfigModel> verifyRulesConfigModelList = VerifyRuleSingleton.getInstance().getVal(route, loadVerifyRule);
+            requestParameterCheck((Map)args, verifyRulesConfigModelList);
+        }
+    }
 }
