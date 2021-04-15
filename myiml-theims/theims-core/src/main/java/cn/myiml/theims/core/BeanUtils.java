29183e69cfe3be201f21c3d311820407d0c47ff3
yangzhoucyl <cleanlove7@163.com>
2021-04-15 09:09:21 +0800

    Bean工具

---
+++ myiml-theims/theims-core/src/main/java/cn/myiml/theims/core/BeanUtils.java
@@ -0,0 +1,33 @@
+package cn.myiml.theims.core;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yangzhou
+ */
+public class BeanUtils {
+
+    public static Map<String, Object> objectToMap(Object obj) throws Exception {
+        if(obj == null){
+            return null;
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor property : propertyDescriptors) {
+            String key = property.getName();
+            if (key.compareToIgnoreCase("class") == 0) {
+                continue;
+            }
+            Method getter = property.getReadMethod();
+            Object value = getter!=null ? getter.invoke(obj) : null;
+            map.put(key, value);
+        }
+        return map;
+    }
+}
