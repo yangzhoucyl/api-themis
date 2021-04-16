5990a543ed4b475e5e9b9331f04ce278b2e9f9a6
yangzhoucyl <cleanlove7@163.com>
2021-04-16 14:44:43 +0800

    新增错误信息常量

---
+++ myiml-theims/theims-core/src/main/java/cn/myiml/theims/core/enums/ErrorMessage.java
@@ -0,0 +1,13 @@
+package cn.myiml.theims.core.enums;
+
+/**
+ * @author yangzhou
+ */
+public class ErrorMessage {
+
+    public static final String NOT_NULL = "参数不能为空";
+
+    public static final String ILLEGAL = "参数不合法!";
+
+    public static final String DEFAULT = "";
+}
