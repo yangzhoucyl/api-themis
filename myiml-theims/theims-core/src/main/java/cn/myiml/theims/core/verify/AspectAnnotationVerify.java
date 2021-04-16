dbcd24a082a34d9bd3dd1b6c1ec50efd365d098b
yangzhoucyl <cleanlove7@163.com>
2021-04-16 14:44:01 +0800

    修改注解切面 获取方法逻辑 及优化

--- myiml-theims/theims-core/src/main/java/cn/myiml/theims/core/verify/AspectAnnotationVerify.java
+++ myiml-theims/theims-core/src/main/java/cn/myiml/theims/core/verify/AspectAnnotationVerify.java
@@ -1,20 +1,22 @@
 package cn.myiml.theims.core.verify;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.myiml.theims.core.BeanUtils;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * 参数验证注解拦截
@@ -32,16 +34,15 @@ public class AspectAnnotationVerify {
     @Around(value = "annotationVerify()")
     public Object annotationVerifyExecute(ProceedingJoinPoint joinPoint) throws Throwable {
         // 获取方法类型
-        try {
-            Map<String, Object> args = getFieldsName(joinPoint);
-            String TypeName = joinPoint.getTarget().getClass().getName();
-            String methodName = joinPoint.getSignature().getName();
-            String routeName = TypeName + "&" + methodName;
-            AbstractParamVerify paramVerify = new AnnotationVerify();
-            paramVerify.verify(args, routeName);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+       Object args = getFieldsName(joinPoint);
+        if (((Map)args).size() == 0){
+            args = joinPoint.getArgs()[0];
         }
+        String TypeName = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        String routeName = TypeName + "&" + methodName;
+        AbstractParamVerify paramVerify = new AnnotationVerify();
+        paramVerify.verify(args, routeName);
         return joinPoint.proceed();
     }
 
@@ -60,33 +61,48 @@ public class AspectAnnotationVerify {
 
 
     @SneakyThrows
-    private static Map<String, Object> getFieldsName(ProceedingJoinPoint joinPoint) throws ClassNotFoundException, NoSuchMethodException {
-        String classType = joinPoint.getTarget().getClass().getName();
-        String methodName = joinPoint.getSignature().getName();
-        // 参数值
+    private Map<String, Object> getFieldsName(ProceedingJoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
-        Class<?>[] classes = new Class[args.length];
-        for (int k = 0; k < args.length; k++) {
-            if (!args[k].getClass().isPrimitive()) {
-                // 获取的是封装类型而不是基础类型
-                String result = args[k].getClass().getName();
-                Class s = map.get(result);
-                classes[k] = s == null ? args[k].getClass() : s;
-            }
+        Signature sig = joinPoint.getSignature();
+        MethodSignature msig = null;
+        if (!(sig instanceof MethodSignature)) {
+            throw new IllegalArgumentException("该注解只能用于方法");
         }
+        msig = (MethodSignature) sig;
+        Object target = joinPoint.getTarget();
+        Method currentMethod = target.getClass().getMethod(msig.getName(), msig.getParameterTypes());
         ParameterNameDiscoverer pnd = new DefaultParameterNameDiscoverer();
-        // 获取指定的方法,第二个参数可以不传，但是为了防止有重载的现象，还是需要传入参数的类型
-        Method method = Class.forName(classType).getMethod(methodName, classes);
+        // 获取指定的方法,为了防止有重载的现象，需要传入参数的类型
         // 参数名
-        String[] parameterNames = pnd.getParameterNames(method);
+        String[] parameterNames = pnd.getParameterNames(currentMethod);
         // 通过map封装参数和参数值
         Map<String, Object> paramMap = new HashMap<>(8);
-        if (args != null && args.length == 1 && args[0] instanceof Object){
-            return BeanUtils.objectToMap(args[0]);
+        if (args.length == 1 && args[0] != null && isValidObject(args[0])){
+            return BeanUtil.beanToMap(args[0]);
         }
         for (int i = 0; i < Objects.requireNonNull(parameterNames).length; i++) {
             paramMap.put(parameterNames[i], args[i]);
         }
         return paramMap;
     }
+
+    private boolean isValidObject(Object arg){
+        Class<?> clazzType = map.get(arg.getClass());
+        if (clazzType != null){
+            if (clazzType.isPrimitive()){
+                return false;
+            }
+        }
+        if (BeanUtil.isBean(arg.getClass())){
+            return true;
+        }
+        if (arg instanceof Map){
+            return true;
+        }
+        if (arg instanceof Iterable){
+            return true;
+        }
+        return false;
+    }
+
 }
