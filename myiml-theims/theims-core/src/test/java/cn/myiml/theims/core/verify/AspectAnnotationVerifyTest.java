1590e016c320c62dfc208950cbf29f2ba2be5071
yangzhoucyl <cleanlove7@163.com>
2021-04-16 14:44:20 +0800

    注解切面单元测试覆盖

--- myiml-theims/theims-core/src/test/java/cn/myiml/theims/core/verify/AspectAnnotationVerifyTest.java
+++ myiml-theims/theims-core/src/test/java/cn/myiml/theims/core/verify/AspectAnnotationVerifyTest.java
@@ -11,6 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:META-INF/spring-aop-verify.xml")
 public class AspectAnnotationVerifyTest {
@@ -63,5 +68,43 @@ public class AspectAnnotationVerifyTest {
         verifyTest.annotationVerifyFieldObj(overview);
     }
 
+    @Test
+    public void annotationVerifyFieldPrimitiveIsTrue(){
+        verifyTest.annotationVerifyFieldPrimitive(1);
+    }
+
+    @Test
+    public void annotationVerifyFieldListIsTrue(){
+        List<QueryDepositOverviewDTO> list = new ArrayList<>();
+        QueryDepositOverviewDTO overview = new QueryDepositOverviewDTO();
+        overview.setCompanyId(111L);
+        list.add(overview);
+        verifyTest.annotationVerifyFieldPrimitive(list);
+    }
+    @Test
+    public void annotationVerifyFieldMapIsTrue(){
+        Map<String,Object> result = new HashMap<>();
+        result.put("companyId", 111L);
+        verifyTest.annotationVerifyFieldMap(result);
+    }
+
+    @Test
+    public void annotationVerifyFieldMapIsFalse(){
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("companyId is Illegal parameter");
+        Map<String,Object> result = new HashMap<>();
+        result.put("companyId", "");
+        verifyTest.annotationVerifyFieldMap(result);
+    }
+
+    @Test
+    public void annotationVerifyFileList(){
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("companyId is Illegal parameter");
+        List<String> list = new ArrayList<>();
+        list.add("111");
+        list.add("xxx");
+        verifyTest.annotationVerifyFieldListPrimitive(list);
+    }
 
 }
