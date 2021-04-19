3fcb3f4c5fd62834432af308a49b4f140cd94042
yangzhoucyl <cleanlove7@163.com>
2021-04-19 15:23:35 +0800

    常量更新

---
+++ myiml-theims/theims-core/src/main/java/cn/myiml/theims/core/enums/TheimsConstants.java
@@ -0,0 +1,13 @@
+package cn.myiml.theims.core.enums;
+
+/**
+ * 常量表
+ * @author yangzhou
+ */
+public class TheimsConstants {
+
+    /**
+     * 错误消息体会字符
+     */
+    public static final String ERROR_MESSAGE_RP = "{param}";
+}
